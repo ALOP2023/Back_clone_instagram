5390c18a40c3856ad5e2e7795ac5e9da30f3763d
ADRIANA ORTIZ <143130009+ALOP2023@users.noreply.github.com>
2025-08-21 22:31:08 -0500

    variables de entorno

--- src/config/Database.php
+++ src/config/Database.php
@@ -7,11 +7,17 @@ use PDOException;
 class Database {
     private static $instance = null;
     public static function getConnection() {
+        $DB_HOST = $ENV['DB_HOST'];
+        $DB_USER = $ENV['DB_USER'];
+        $DB_PASSWORD = $ENV['DB_PASSWORD'];
+        $DB_NAME = $ENV['DB_NAME'];
+        $DB_PORT = $ENV['DB_PORT'];
         // var_dump(self::$instance);
         if (self::$instance === null) {
             // var_dump("se hizo la conexiòn");
             try {
-                self::$instance = new PDO("mysql:host=tramway.proxy.rlwy.net;port=32486;dbname=instagram_clone", "root", "NpGulXAsbGcguCrILrNmvZFuBJvseVbV");
+                // self::$instance = new PDO("mysql:host=tramway.proxy.rlwy.net;port=32486;dbname=instagram_clone", "root", "NpGulXAsbGcguCrILrNmvZFuBJvseVbV");
+                self::$instance = new PDO("mysql:$DB_HOST;port=$DB_PORT;dbname=$DB_NAME", "$DB_USER", "$DB_PASSWORD");
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 die("Error DB: " . $e->getMessage());
---
+++ src/index.php
@@ -0,0 +1,3 @@
+<?php
+phpinfo();
+echo "Backend de Instagram Clone funcionando ";
